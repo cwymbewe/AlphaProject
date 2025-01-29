d81386ad7d63f6d8504bdea7fd7a8c39962f98fd
cwymbewe <cwymbewe@gmail.com>
2025-01-30 01:40:18 +0200

    Add Tailwind Config File

---
+++ alpha_inventory_system/tailwind.config.js
@@ -0,0 +1,9 @@
+// module.exports = {
+//   content: [
+//     "./src/**/*.{js,jsx,ts,tsx}", // Adjust the paths as necessary
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
